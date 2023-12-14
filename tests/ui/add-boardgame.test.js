51ab0e13b2c9c103927fbb39120a41fe4fd939e7
TsvetomirBahov <tsvetomir.bahov@gmail.com>
2023-12-14 18:57:30 +0200

    fix url in UI tests

--- tests/ui/add-boardgame.test.js
+++ tests/ui/add-boardgame.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add boardgame page', async ({ page }) => {
-    await page.goto('https://boardgames-d9q3.onrender.com//add-boardgame');
+    await page.goto('https://boardgames-d9q3.onrender.com/add-boardgame');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('https://boardgames-d9q3.onrender.com//');
+    await page.goto('https://boardgames-d9q3.onrender.com/');
     const heading = await page.$('h1');
     const text = await heading.textContent();
     expect(text).toContain('Boardgames Collection');  
